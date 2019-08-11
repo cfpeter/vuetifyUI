ed0554fc43f4ac92aa5bf351b68ccb8f8dad53e0
cfbedo@yahoo.com <cfbedo@Yahoo.com>
2019-08-10 19:29:28 -0700

    vuetify added

--- src/router.js
+++ src/router.js
@@ -1,26 +1,48 @@
 import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Router from "vue-router"; 
+import Register from './views/auth/Register.vue'
+import Login from './views/auth/Login.vue'
+import Logout from './views/auth/Logout.vue'
+import Dashboard from './views/Dashboard.vue';
+import Setting from './views/Setting.vue';
+
 
 Vue.use(Router);
 
 export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
-  routes: [
+  routes: [ 
     {
-      path: "/",
-      name: "home",
-      component: Home
+      path: '/register' , 
+      name: 'register',
+      component: Register , 
+      meta: { requiresVisitor: true }
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: '/login' , 
+      name: 'login',
+      component: Login , 
+      meta: { requiresVisitor: true }
+    },
+    {
+      path: '/logout' , 
+      name: 'logout',
+      component: Logout , 
+      // meta: { requiresVisitor: true }
+    },
+    
+    {
+      path: '/dashboard' , 
+      name: 'dashboard',
+      component: Dashboard , 
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/setting' , 
+      name: 'setting',
+      component: Setting , 
+      meta: { requiresAuth: true }
     }
   ]
 });
